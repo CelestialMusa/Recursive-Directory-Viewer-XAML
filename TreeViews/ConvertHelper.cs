0aad9153f94e0aa370e30f13809ed0b4b319db12
CelestialMusa <Zakes.musa@hotmail.com>
2017-06-27 09:11:20 +0200

    Initial commit - Missing Folder Icon

---
+++ TreeViews/ConvertHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeViews
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class ConvertHelper : IValueConverter
+    {
+        public static ConvertHelper sInstance = new ConvertHelper();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TreeViews/MainWindow.xaml.cs
+++ TreeViews/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TreeViews
 {
@@ -20,9 +12,86 @@ namespace TreeViews
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+                item.Items.Add(null);
+
+                item.Expanded += ExpandedFolder;
+
+                folderView.Items.Add(item);
+            }
+        }
+
+        private void ExpandedFolder(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            //Remove empty items.
+            item.Items.Clear();
+
+            var path = (string)item.Tag;
+
+            var dis = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(path);
+                if (dirs.Length > 0)
+                    dis.AddRange(dirs);
+            }
+            catch(Exception ex)  { Console.WriteLine(ex.Message); }
+
+            dis.ForEach(x =>  
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = getFolderName(x),
+                    Tag = x
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += ExpandedFolder;
+
+                item.Items.Add(subItem);
+            });
+        }
+
+        public static string getFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+
+            path = path.Replace('/', '\\');
+
+            if (path.LastIndexOf('\\') < 0)
+                return string.Empty;
+
+            return path.Substring(path.LastIndexOf('\\')+1);
         }
     }
 }
